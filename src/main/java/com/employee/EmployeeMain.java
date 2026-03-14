6eeaa23cb8e6d72fdbad205d715404751d7117e2
VignejanBridgeLabz <vignejan.sivanesan@bridgelabz.com>
2026-03-14 14:45:00 +0530

    UC4: Calculating Wages

--- src/main/java/com/employee/EmployeeMain.java
+++ src/main/java/com/employee/EmployeeMain.java
@@ -3,11 +3,11 @@ package com.employee;
 import java.util.Scanner;
 
 /**
- * UC3 - Solving using
+ * UC4 - Calculating Wages
  *
  * Requirements:
-     *   - Switch Case
-     *   - Statement
+     *   - for a Month
+     *   - Assume 20 Working Day per Month
  *
  * @author  VignejanBridgeLabz
  * @version 1.0
@@ -17,10 +17,24 @@ public class EmployeeMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee");
         
-        System.out.println("\n=== UC3: Solving using ===");
-        // TODO: Implement UC3
-        // 
-Switch Case        // Statement
-        System.out.println("UC3 - Implementation pending");
+        System.out.println("\n=== UC4: Calculating Wages ===");
+        final int WAGE_PER_HOUR   = 20;
+        final int FULL_DAY_HOURS  = 8;
+        final int PART_TIME_HOURS = 4;
+        final int WORKING_DAYS    = 20;
+        final int MAX_HOURS       = 100;
+
+        int totalWage = 0, totalHours = 0, day = 0;
+        java.util.Random rand = new java.util.Random();
+
+        while (totalHours < MAX_HOURS && day < WORKING_DAYS) {
+            int attendance = rand.nextInt(3); // 0=absent,1=full,2=part
+            int hoursWorked = (attendance == 1) ? FULL_DAY_HOURS
+                            : (attendance == 2) ? PART_TIME_HOURS : 0;
+            totalWage  += hoursWorked * WAGE_PER_HOUR;
+            totalHours += hoursWorked;
+            day++;
+        }
+        System.out.println("Total Wage: " + totalWage + " | Days: " + day + " | Hours: " + totalHours);
     }
 }
